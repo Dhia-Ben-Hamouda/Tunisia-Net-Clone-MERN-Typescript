c4d1e4e696057188471c05142c70a3d74061a6b8
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2023-03-04 16:38:25 +0100

    setup project and working on home page

--- client/src/pages/ForgetPassword.tsx
+++ client/src/pages/ForgetPassword.tsx
@@ -1,25 +1,9 @@
 import React from "react";
-import { TextField } from "@mui/material";
-import Button from "../components/Button";
 
 export default function(){
-
-    async function submitHandler(e: React.FormEvent){
-        e.preventDefault();
-
-        
-    }
-
     return(
         <>
-            <section id="forget-password">
-                <form onSubmit={submitHandler} autoComplete="off" >
-                    <TextField 
-                        label="enter email"
-                    />
-                    <Button type="submit" text="Send password reset link" />
-                </form>
-            </section>
+            
         </>
     )
 }
